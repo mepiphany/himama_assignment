932b942a1bb04f0c60b98d79d8286f7fe979aaf3
mepiphany <mike.epiphany@gmail.com>
2022-04-25 10:07:17 -0700

    add clock in clock out

--- app/models/time_sheet.rb
+++ app/models/time_sheet.rb
@@ -8,25 +8,25 @@ class TimeSheet < ApplicationRecord
   validate :clock_out_date_cannot_be_before_clock_in_date
 
   def clock_in_date_cannot_be_from_the_past
-    if clock_in < Date.today
+    if clock_in.present? && clock_in < Date.today
       errors.add(:clock_in, "can't be in the past")
     end
   end
 
   def clock_in_date_cannot_be_after_clock_out_date
-    if clock_in > clock_out 
+    if (clock_out.present? && clock_in.present?) && clock_in > clock_out  
       errors.add(:clock_in, "can't be after clock out date")
     end
   end
 
   def clock_out_date_cannot_be_from_the_past
-    if clock_out < Date.today
+    if clock_out.present? && clock_out < Date.today
       errors.add(:clock_out, "can't be in the past")
     end
   end
 
   def clock_out_date_cannot_be_before_clock_in_date
-    if clock_out < clock_in 
+    if (clock_out.present? && clock_in.present?) && clock_out < clock_in 
       errors.add(:clock_out, "can't be after clock out date")
     end
   end
