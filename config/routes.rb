155dca70045cd46afd8e822f73e777153b2559d0
mepiphany <mike.epiphany@gmail.com>
2022-04-25 07:08:05 -0700

    add devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "time_sheets#index" 
   resources :time_sheets
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
